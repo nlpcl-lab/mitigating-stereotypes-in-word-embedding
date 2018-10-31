4ffe3fc76b7f2166cb8f9a881ca9f06da8e1c5ef
huijelee <jae4258@naver.com>
2018-10-31 16:32:43 +0900

    Sent Entity Embedding construction: Initialization and modeling

--- sentgen_we.py
+++ sentgen_we.py
@@ -1,21 +1,6 @@
 # -*- coding: utf-8 -*-
 
-DEFAULT_ARGUMENTS = dict(
-        # for iterative graph algorithms
-        similarity_power=1,
-        arccos=True,
-        max_iter=50,
-        epsilon=1e-6,
-        sym=True,
-
-        # for learning embeddings transformation
-        n_epochs=50,
-        force_orthogonal=False,
-        batch_size=100,
-        cosine=False,
-
-        ## bootstrap
-        num_boots=1,
-        n_procs=1,
-)
+# Base sentiment words (same as Hamilton et al. 2016)
+POSITIVE_HIST = ["good", "lovely", "excellent", "fortunate", "pleasant", "delightful", "perfect", "loved", "love", "happy"]
+NEGATIVE_HIST = ["bad", "horrible", "poor",  "unfortunate", "unpleasant", "disgusting", "evil", "hated", "hate", "unhappy"]
 
